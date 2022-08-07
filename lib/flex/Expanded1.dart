9047c890b699e1f217773997ca8503063a2c900b
Virak459 <virakoumst@gmail.com>
2022-08-07 11:53:44 +0700

    Update Expanded1.dart

--- lib/flex/Expanded1.dart
+++ lib/flex/Expanded1.dart
@@ -9,136 +9,139 @@ class exercise1 extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(),
       body: Container(
-          child: StaggeredGrid.count(
-        crossAxisCount: 4,
-        mainAxisSpacing: 4,
-        crossAxisSpacing: 5,
-        children: [
-          StaggeredGridTile.count(
-            crossAxisCellCount: 2,
-            mainAxisCellCount: 2,
-            child: Container(
-              color: Colors.amber,
-              child: Icon(
-                Icons.window,
-                color: Colors.white,
+        color: Colors.brown,
+        margin: EdgeInsets.all(10),
+        child: StaggeredGrid.count(
+          crossAxisCount: 4,
+          mainAxisSpacing: 4,
+          crossAxisSpacing: 5,
+          children: [
+            StaggeredGridTile.count(
+              crossAxisCellCount: 2,
+              mainAxisCellCount: 2,
+              child: Container(
+                color: Colors.amber,
+                child: Icon(
+                  Icons.window,
+                  color: Colors.white,
+                ),
               ),
             ),
-          ),
-          StaggeredGridTile.count(
-            crossAxisCellCount: 2,
-            mainAxisCellCount: 1,
-            child: Container(
-              color: Colors.blue,
-              child: Icon(
-                Icons.window,
-                color: Colors.white,
+            StaggeredGridTile.count(
+              crossAxisCellCount: 2,
+              mainAxisCellCount: 1,
+              child: Container(
+                color: Colors.blue,
+                child: Icon(
+                  Icons.window,
+                  color: Colors.white,
+                ),
               ),
             ),
-          ),
-          StaggeredGridTile.count(
-            crossAxisCellCount: 2,
-            mainAxisCellCount: 3,
-            child: Container(
-              child: StaggeredGrid.count(
-                crossAxisCount: 4,
-                mainAxisSpacing: 4,
-                crossAxisSpacing: 5,
-                children: [
-                  StaggeredGridTile.count(
-                    crossAxisCellCount: 2,
-                    mainAxisCellCount: 4,
-                    child: Container(
-                      color: Colors.blue,
-                      child: Icon(
-                        Icons.window,
-                        color: Colors.white,
+            StaggeredGridTile.count(
+              crossAxisCellCount: 2,
+              mainAxisCellCount: 3,
+              child: Container(
+                child: StaggeredGrid.count(
+                  crossAxisCount: 4,
+                  mainAxisSpacing: 4,
+                  crossAxisSpacing: 5,
+                  children: [
+                    StaggeredGridTile.count(
+                      crossAxisCellCount: 2,
+                      mainAxisCellCount: 4,
+                      child: Container(
+                        color: Colors.blue,
+                        child: Icon(
+                          Icons.window,
+                          color: Colors.white,
+                        ),
                       ),
                     ),
-                  ),
-                  StaggeredGridTile.count(
-                    crossAxisCellCount: 2,
-                    mainAxisCellCount: 2,
-                    child: Container(
-                      color: Colors.blue,
-                      child: Icon(
-                        Icons.window,
-                        color: Colors.white,
+                    StaggeredGridTile.count(
+                      crossAxisCellCount: 2,
+                      mainAxisCellCount: 2,
+                      child: Container(
+                        color: Colors.blue,
+                        child: Icon(
+                          Icons.window,
+                          color: Colors.white,
+                        ),
                       ),
                     ),
-                  ),
-                  StaggeredGridTile.count(
-                    crossAxisCellCount: 2,
-                    mainAxisCellCount: 4,
-                    child: Container(
-                      color: Colors.blue,
-                      child: Icon(
-                        Icons.window,
-                        color: Colors.white,
+                    StaggeredGridTile.count(
+                      crossAxisCellCount: 2,
+                      mainAxisCellCount: 4,
+                      child: Container(
+                        color: Colors.blue,
+                        child: Icon(
+                          Icons.window,
+                          color: Colors.white,
+                        ),
                       ),
                     ),
-                  ),
-                  StaggeredGridTile.count(
-                    crossAxisCellCount: 2,
-                    mainAxisCellCount: 2,
-                    child: Container(
-                      color: Colors.blue,
-                      child: Icon(
-                        Icons.window,
-                        color: Colors.white,
+                    StaggeredGridTile.count(
+                      crossAxisCellCount: 2,
+                      mainAxisCellCount: 2,
+                      child: Container(
+                        color: Colors.blue,
+                        child: Icon(
+                          Icons.window,
+                          color: Colors.white,
+                        ),
                       ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
-          ),
-          StaggeredGridTile.count(
-            crossAxisCellCount: 2,
-            mainAxisCellCount: 2,
-            child: Container(
-              color: Colors.blue,
-              child: Icon(
-                Icons.window,
-                color: Colors.white,
+            StaggeredGridTile.count(
+              crossAxisCellCount: 2,
+              mainAxisCellCount: 2,
+              child: Container(
+                color: Colors.blue,
+                child: Icon(
+                  Icons.window,
+                  color: Colors.white,
+                ),
               ),
             ),
-          ),
-          StaggeredGridTile.count(
-            crossAxisCellCount: 3,
-            mainAxisCellCount: 1,
-            child: Container(
-              color: Colors.blue,
-              child: Icon(
-                Icons.window,
-                color: Colors.white,
+            StaggeredGridTile.count(
+              crossAxisCellCount: 3,
+              mainAxisCellCount: 1,
+              child: Container(
+                color: Colors.blue,
+                child: Icon(
+                  Icons.window,
+                  color: Colors.white,
+                ),
               ),
             ),
-          ),
-          StaggeredGridTile.count(
-            crossAxisCellCount: 1,
-            mainAxisCellCount: 1,
-            child: Container(
-              color: Colors.blue,
-              child: Icon(
-                Icons.window,
-                color: Colors.white,
+            StaggeredGridTile.count(
+              crossAxisCellCount: 1,
+              mainAxisCellCount: 1,
+              child: Container(
+                color: Colors.blue,
+                child: Icon(
+                  Icons.window,
+                  color: Colors.white,
+                ),
               ),
             ),
-          ),
-          StaggeredGridTile.count(
-            crossAxisCellCount: 4,
-            mainAxisCellCount: 1,
-            child: Container(
-              color: Colors.blue,
-              child: Icon(
-                Icons.window,
-                color: Colors.white,
+            StaggeredGridTile.count(
+              crossAxisCellCount: 4,
+              mainAxisCellCount: 1,
+              child: Container(
+                color: Colors.blue,
+                child: Icon(
+                  Icons.window,
+                  color: Colors.white,
+                ),
               ),
             ),
-          ),
-        ],
-      )),
+          ],
+        ),
+      ),
     );
   }
 }
